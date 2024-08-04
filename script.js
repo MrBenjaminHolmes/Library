ee5129edf27a44a22b9315eec87c10ee3b997a27
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-08-04 13:04:12 +0100

    Added Delete Button Function

--- script.js
+++ script.js
@@ -21,42 +21,58 @@ document.getElementById("submitBtn").addEventListener("click", function() {
 
 const myLibrary = [];
 
-function Book(name,author,pages,img,status) {
+function Book(name, author, pages, img, status) {
+  this.id = myLibrary.length;
   this.name = name;
   this.author = author;
   this.pages = pages;
   this.img = img;
-  this.status = status
+  this.status = status;
   myLibrary.push(this);
   displayBook();
 }
 
-const harryPotter = new Book("Harry Potter and the Sorcerer's Stone","J.K. Rowling",235,"https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_568,c_scale/jackets/9781408855652.jpg");
+const harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 235, "https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_568,c_scale/jackets/9781408855652.jpg", "off");
 
-
-function displayBook(){
+function displayBook() {
   const booksContainer = document.getElementById("bookGrid");
+
   booksContainer.innerHTML = "";
 
-  myLibrary.forEach((book)=>{
-      const bookDiv = document.createElement("div");
-      bookDiv.classList.add("book");
-      bookDiv.innerHTML = `
-            <img class="cover" src="${book.img}" alt="">
-            <h2 class="bookTitle">${book.name}</h2>
-            <p class="author">${book.author}</p>
-            <p class="pages">${book.pages} Pages</p>
-            <div class="menu">
-                <button type="button" class="readButton"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"/></svg>READ </button>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>
-            </div>`;
+  myLibrary.forEach((book) => {
+    console.table(myLibrary);
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    bookDiv.innerHTML = `
+      <img class="cover" src="${book.img}" alt="">
+      <h2 class="bookTitle">${book.name}</h2>
+      <p class="author">${book.author}</p>
+      <p class="pages">${book.pages} Pages</p>
+      <div class="menu">
+        <button type="button" class="readButton ${book.status}"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"/></svg>READ </button>
+        <svg id="${book.id}" class="bin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>
+      </div>`;
+    booksContainer.appendChild(bookDiv);
+  });
 
-        booksContainer.appendChild(bookDiv);
-
-  })
+  addEventListenersToBooks();
 }
 
-const form = document.querySelector("form")
+function addEventListenersToBooks() {
+  const binBtns = document.querySelectorAll(".menu > .bin");
+
+  binBtns.forEach(button => {
+    button.addEventListener("click", function() {
+        const index = myLibrary.indexOf(myLibrary.find(book => book.id = button.id));
+        if (index > -1) { 
+          myLibrary.splice(index, 1); 
+          displayBook();
+        }
+    });
+  });
+}
+
+const form = document.querySelector("form");
 
 form.addEventListener("submit", function(e) {
   e.preventDefault();
@@ -76,7 +92,11 @@ form.addEventListener("submit", function(e) {
     };
     reader.readAsDataURL(file);
   } else {
-    const bookObject = new Book(bookData.name, bookData.author, bookData.pages, "/images/noCover.png", bookData.status);
+    if (bookData.status === undefined) {
+      const bookObject = new Book(bookData.name, bookData.author, bookData.pages, "/images/noCover.png", "off");
+    } else {
+      const bookObject = new Book(bookData.name, bookData.author, bookData.pages, "/images/noCover.png", bookData.status);
+    }
   }
   form.reset();
 });
