f6d6fe54442489a8aee76dc4925ad20bee8048a8
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-08-04 14:19:38 +0100

    Added Status Toggle

--- script.js
+++ script.js
@@ -32,8 +32,6 @@ function Book(name, author, pages, img, status) {
   displayBook();
 }
 
-const harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 235, "https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_568,c_scale/jackets/9781408855652.jpg", "off");
-
 function displayBook() {
   const booksContainer = document.getElementById("bookGrid");
 
@@ -49,28 +47,60 @@ function displayBook() {
       <p class="author">${book.author}</p>
       <p class="pages">${book.pages} Pages</p>
       <div class="menu">
-        <button type="button" class="readButton ${book.status}"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"/></svg>READ </button>
+        <button type="button" class="readButton ${book.status}" id="read-${book.id}">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"/></svg>
+          <span>${book.status === "on" ? "READ" : "UNREAD"}</span>
+        </button>
         <svg id="${book.id}" class="bin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>
       </div>`;
     booksContainer.appendChild(bookDiv);
   });
 
   addEventListenersToBooks();
+  addReadButtonListeners();
 }
+
 
 function addEventListenersToBooks() {
   const binBtns = document.querySelectorAll(".menu > .bin");
 
   binBtns.forEach(button => {
     button.addEventListener("click", function() {
-        const index = myLibrary.indexOf(myLibrary.find(book => book.id = button.id));
-        if (index > -1) { 
-          myLibrary.splice(index, 1); 
-          displayBook();
-        }
+      const index = myLibrary.findIndex(book => book.id == button.id);
+      if (index > -1) { 
+        myLibrary.splice(index, 1); 
+        displayBook();
+      }
     });
   });
 }
+
+function addReadButtonListeners() {
+  const readBtns = document.querySelectorAll(".menu > .readButton");
+
+  readBtns.forEach(button => {
+    button.addEventListener("click", function() {
+      const id = parseInt(button.id.replace('read-', ''));
+      const book = myLibrary.find(book => book.id === id);
+
+      if (book) {
+        const span = button.querySelector("span");
+        if (book.status === "off") {
+          book.status = "on";
+          button.classList.remove("off");
+          button.classList.add("on");
+          span.textContent = "READ";
+        } else {
+          book.status = "off";
+          button.classList.remove("on");
+          button.classList.add("off");
+          span.textContent = "UNREAD";
+        }
+      }
+    });
+  });
+}
+
 
 const form = document.querySelector("form");
 
@@ -88,15 +118,17 @@ form.addEventListener("submit", function(e) {
     const reader = new FileReader();
     reader.onload = function(event) {
       const imgURL = event.target.result;
-      const bookObject = new Book(bookData.name, bookData.author, bookData.pages, imgURL, bookData.status);
+      new Book(bookData.name, bookData.author, bookData.pages, imgURL, bookData.status);
     };
     reader.readAsDataURL(file);
   } else {
     if (bookData.status === undefined) {
-      const bookObject = new Book(bookData.name, bookData.author, bookData.pages, "/images/noCover.png", "off");
+      new Book(bookData.name, bookData.author, bookData.pages, "/images/noCover.png", "off");
     } else {
-      const bookObject = new Book(bookData.name, bookData.author, bookData.pages, "/images/noCover.png", bookData.status);
+      new Book(bookData.name, bookData.author, bookData.pages, "/images/noCover.png", bookData.status);
     }
   }
   form.reset();
 });
+
+displayBook();
